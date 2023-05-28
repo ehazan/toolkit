e03b8fd96f3c8f7af103dd0b44af7b6ee4c883ff
Evgeny Hazan <evgenyh@starsgroup.com>
2023-05-28 10:00:33 -0400

    Initial entry

---
+++ tools.go
@@ -0,0 +1,20 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// Tools is the type used to instantiate this module
+type Tools struct{}
+
+// RandomString Random string return random characheter string
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
