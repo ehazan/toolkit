0eb1831cdec7c0b9adbbd992d55d662920e7c769
Evgeny Hazan <evgenyh@flutterint.com>
2024-05-29 09:21:10 -0400

    Initial commit

---
+++ tools.go
@@ -0,0 +1,20 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is a collection of useful tools
+type Tools struct {}
+
+// RandomString generates a random string of length n
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
